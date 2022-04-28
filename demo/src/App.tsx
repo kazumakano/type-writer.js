afeefbde0c79baaf56adb3452a26b55d0ef52d9d
kazumakano <kazumakano@outlook.jp>
2022-04-28 19:52:23 +0900

    update demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,14 +3,12 @@ import useTypeWriter from "./type-writer"
 
 
 export default function App(): JSX.Element {
-  const tw = useTypeWriter("Hello, world!\nThis is demo of type-writer.js.")
+  const tw = useTypeWriter("Hello, world!\nThis is demo site of type-writer.js.")
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <pre>{tw}</pre>
-        </p>
+        <pre>{tw}</pre>
       </header>
     </div>
   )
